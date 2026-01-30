d891143c3f17eeffe0ea6a84dee5c844a7d2c5e5
Noham-Sustek <nohamsustek.pro@gmail.com>
2026-01-30 15:43:51 +0100

    Commit1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import ui.MainWindow;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainWindow mainWindow = new MainWindow(primaryStage);
+        mainWindow.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
